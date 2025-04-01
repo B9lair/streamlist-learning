d8ed41f5a31a25ac2e5d3355090c4581cad948ca
nie <2147511692@qq.com>
2025-04-02 00:36:13 +0800

    app with  chat elements

--- home.py
+++ home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from langchain_openai import ChatOpenAI
-from langchain.schema import HumanMessage
+from langchain.schema import (HumanMessage,AIMessage)
 
 
 # 初始化ChatOpenAI对象
@@ -24,17 +24,45 @@ st.set_page_config(page_title="Welcome to ASL", layout="wide")
 # 页面标题
 st.title("👍Welcome to wxy")
 
+
+# 维护会话状态列表,用message这个键值，管理所有的历史消息
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+
+# 固定在最底部
+prompt = st.chat_input("Type something...")
+
 # 当chat实例不为空,完成实例
 if chat:
     with st.container():
         st.header("Chat with GPT")
-        prompt = st.text_input("Prompt", value="", max_chars=None, key=None, type='default')
+
+        for message in st.session_state["messages"]:
+            if isinstance(message, HumanMessage):
+                with st.chat_message("user"):
+                    st.markdown(message.content)
+            elif isinstance(message, AIMessage):
+                with st.chat_message("assistant"):
+                    st.markdown(message.content)
+
+        # prompt = st.text_input("Prompt", value="", max_chars=None, key=None, type='default')
+
         # 提交按钮
-        asked = st.button("Ask")
-        if asked:
+        #asked = st.button("Ask")
+        #if asked:
+        # 实现历史对话
+        if prompt:
+            # 当提问时追加提问的历史消息
+            st.session_state["messages"].append(HumanMessage(content=prompt))
+            with st.chat_message("user"):
+                st.markdown(prompt)
             ai_message = chat([HumanMessage(content=prompt)])
-            # 开心
-            st.write(ai_message.content)
+            # 追加回答的历史消息
+            st.session_state["messages"].append(ai_message)
+            with st.chat_message("assistant"):
+                st.markdown(ai_message.content)
+
 
 else:
     with st.container():
